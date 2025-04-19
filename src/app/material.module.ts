8174987220d7194c65804789384301a692aca113
Rahele <108678700+RaheleAh77@users.noreply.github.com>
2025-04-20 00:25:46 +0330

    develope new-training tab

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,6 +10,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [],
   imports: [
@@ -23,7 +25,9 @@ import {MatTabsModule} from '@angular/material/tabs';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule,
+    MatSelectModule
   ],
   exports: [
     MatIconModule,
@@ -36,7 +40,9 @@ import {MatTabsModule} from '@angular/material/tabs';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule,
+    MatSelectModule
   ],
 })
 export class MaterialModule {}
